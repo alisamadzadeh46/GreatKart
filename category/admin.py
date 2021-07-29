e91dd93a94baa14a33051d56ccb0f578a1aaac35
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-07-29 22:01:44 +0430

    update admin.py and models.py add category

--- category/admin.py
+++ category/admin.py
@@ -7,4 +7,5 @@ from .models import *
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['category_name', 'slug']
     list_filter = ['category_name', 'slug']
+    prepopulated_fields = {'slug': ('category_name',)}
     search_fields = ['category_name', 'slug']
