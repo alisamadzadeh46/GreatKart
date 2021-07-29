e86b56221ac3fb0f4825ffc1da5338bf3ab95aba
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-07-30 00:10:46 +0430

    update

--- store/views.py
+++ store/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from store.models import Product
 
 
 def store(request):
+    products = Product.objects.all().filter(is_available=True)
+    context = {
+        'products': products,
+    }
     return render(request, 'store/store.html')
