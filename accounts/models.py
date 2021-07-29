eef8f88ff8b7bbe5ecef3c31b9f7719326f5b510
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-07-29 21:05:37 +0430

    crate Account class

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    username = models.CharField(max_length=120, unique=True)
+    email = models.EmailField(max_length=120, unique=True)
+    phone_number = models.CharField(max_length=120)
+    date_join = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
