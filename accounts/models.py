458650f6fc9434c02328ddf52d7b2fa97119d3b7
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-07-29 21:27:05 +0430

    update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,39 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not username:
+            raise ValueError("User must have a username")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, username, email, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
+
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
@@ -18,5 +51,14 @@ class Account(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
-    def __str__(self):
-        return self.email
+    objects = MyAccountManager()
+
+    @property
+    def name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
