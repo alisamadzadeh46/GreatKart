9a8320d5ddbdfb14d6082c310d9827e31257d6a1
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-07-29 21:45:39 +0430

    fix admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import *
 from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Account)
-# @admin.register(Account)
-# class AccountAdmin(UserAdmin):
-#     ordering = ['id']
+
+@admin.register(Account)
+class AccountAdmin(UserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'first_name', 'last_name', 'username', 'last_login', 'is_active', 'date_join']
+    filter_horizontal = []
+    list_filter = ['email', 'first_name', 'last_name', 'username', 'last_login', 'is_active', 'date_join']
+    search_fields = ['email', 'first_name', 'last_name', 'username', 'last_login', 'is_active', 'date_join']
+    fieldsets = []
