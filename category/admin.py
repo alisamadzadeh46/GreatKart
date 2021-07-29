adcbb29f6bf0a83476c4793b1dbb64c5d9eb04cf
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-07-29 20:19:24 +0430

    create CategoryAdmin class in admin.py update models.py and requirement.txt

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name', 'slug']
+    list_filter = ['category_name', 'slug']
+    search_fields = ['category_name', 'slug']
