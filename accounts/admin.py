0edaf66d358c16c9fca6f16e6721429dde31b9b7
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-07-29 21:36:11 +0430

    temporary admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.register(Account)
+# @admin.register(Account)
+# class AccountAdmin(UserAdmin):
+#     ordering = ['id']
