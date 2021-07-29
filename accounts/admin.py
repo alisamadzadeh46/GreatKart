6e17d8abe15858b1531aa3b5be20ef486ab2de56
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-07-29 21:49:16 +0430

    update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,9 +5,11 @@ from django.contrib.auth.admin import UserAdmin
 
 @admin.register(Account)
 class AccountAdmin(UserAdmin):
-    ordering = ['id']
+    ordering = ['-date_join']
     list_display = ['email', 'first_name', 'last_name', 'username', 'last_login', 'is_active', 'date_join']
     filter_horizontal = []
     list_filter = ['email', 'first_name', 'last_name', 'username', 'last_login', 'is_active', 'date_join']
     search_fields = ['email', 'first_name', 'last_name', 'username', 'last_login', 'is_active', 'date_join']
     fieldsets = []
+    list_display_links = ['email', 'first_name', 'last_name']
+    readonly_fields = ['last_login', 'date_join']
