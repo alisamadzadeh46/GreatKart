9e2a73347b9673a2e2c9f692b7e294068b001a39
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-07-31 19:27:33 +0430

    update admin.py

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['cart_id', 'cart_id']
+    list_filter = ['cart_id', 'cart_id']
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'cart', 'quantity', 'is_active']
+    list_filter = ['quantity', 'is_active']
