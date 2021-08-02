a09f73cd5cfe1309674c2423f884ada91d76e8d4
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-08-02 20:10:11 +0430

    update views.py

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
+from carts.models import Cart
+from store.models import Product
+
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+    cart.save()
+
 
 def cart(request):
     return render(request, 'cart/cart.html')
