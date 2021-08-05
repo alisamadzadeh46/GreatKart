c72055f01dfcac6511cf33ca7945069f4fb91feb
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-08-05 22:47:54 +0430

    update

--- carts/views.py
+++ carts/views.py
@@ -42,7 +42,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-    except objectNotExist:
+    except CartItem.DoesNotExist:
         pass
 
     context = {
