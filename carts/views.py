f0081e7322de89fb4412792e813a13575b77830b
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-08-08 19:07:44 +0430

    update views.py

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from carts.models import Cart, CartItem
 from store.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def _cart_id(request):
@@ -49,6 +50,8 @@ def remove_cart(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -56,7 +59,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (2 * total) / 100
         grand_total = total + tax
-    except CartItem.DoesNotExist:
+    except ObjectDoesNotExist:
         pass
 
     context = {
