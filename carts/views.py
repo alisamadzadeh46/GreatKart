30199ebbc196241fedeb64b6a4a36ad3afecbf19
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-10-23 18:49:01 +0330

    update

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from carts.models import Cart, CartItem
-from store.models import Product
+from store.models import Product, Variation
 from django.core.exceptions import ObjectDoesNotExist
 
 
@@ -14,8 +14,17 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     if request.method == 'POST':
-        color = request.POST['color']
-        size = request.POST['size']
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(variation_category__iexact=key, variation_value__iexact=value)
+            except:
+                pass
+
+        # color = request.POST['color']
+        # size = request.POST['size']
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
