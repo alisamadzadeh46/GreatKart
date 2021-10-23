bcfe433b45f8062aa93d8ef328475f1b63088682
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-10-23 20:16:48 +0330

    update

--- carts/views.py
+++ carts/views.py
@@ -36,9 +36,9 @@ def add_cart(request, product_id):
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
         if len(product_variation) > 0:
-            cart_item.variation.clear()
+            cart_item.variations.clear()
             for item in product_variation:
-                cart_item.variation.add(item)
+                cart_item.variations.add(item)
         cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
@@ -48,9 +48,9 @@ def add_cart(request, product_id):
             cart=cart,
         )
         if len(product_variation) > 0:
-            cart_item.variation.clear()
+            cart_item.variations.clear()
             for item in product_variation:
-                cart_item.variation.add(item)
+                cart_item.variations.add(item)
         cart_item.save()
     return redirect('carts:cart')
 
