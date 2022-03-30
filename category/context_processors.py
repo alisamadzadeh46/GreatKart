f9ccf0df1314114bbcd339c92407bcb99a2d465f
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-03-30 20:48:24 +0430

    update

--- category/context_processors.py
+++ category/context_processors.py
@@ -3,4 +3,5 @@ from .models import Category
 
 def menu_link(request):
     links = Category.objects.all()
-    return dict(links=links)
+    ca = Category.objects.filter(parent=None)[:5]
+    return dict(links=links, ca=ca)
